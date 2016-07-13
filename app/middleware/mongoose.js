eab9bb6362cd2827313bb142378f1aa6b7faefe8
shkherad <shkherad@bu.edu>
2016-07-13 12:34:31 -0400

    require dotenv file in mongoose
    bug fixes for heroku

--- app/middleware/mongoose.js
+++ app/middleware/mongoose.js
@@ -1,7 +1,8 @@
 'use strict';
 
+require('dotenv').config();
 const mongoose = require('mongoose');
-const uri = process.env.MONGOLAB_URI || 'mongodb://localhost/ecommerce-express-api';
+const uri = process.env.MONGOLAB_URI || 'mongodb://localhost/ecommerce-api';
 mongoose.Promise = global.Promise;
 mongoose.connect(uri);
 
